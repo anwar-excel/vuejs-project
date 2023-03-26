58e1a272e594671be7623d8705317808c4dd5e17
unknown <anwarhosen575@gmail.com>
2023-03-26 16:16:22 +0600

    instance

--- app.js
+++ app.js
@@ -182,3 +182,30 @@ new Vue({
     },
    
 });
+
+const app11 = new Vue({
+    el:"#app1",
+    data:{
+        value:"1st"
+    },
+    methods:{
+        changeValue(){
+            app12.value = 'Changed';
+
+        }
+    },
+
+});
+
+const app12 = new Vue({
+    el:"#app2",
+    data:{
+        value:"2nd"
+    },
+    methods:{
+        changeValue(){
+            app11.value = 'Changed';
+
+        }
+    },
+})
